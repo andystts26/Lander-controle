0733c3b73f93b26b26b3253ca77afbf1add3fb04
andystts26 <andersonibc01@gmail.com>
2026-07-14 03:24:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lander-controle-v115";
+const CACHE="lander-controle-v117";
 const ASSETS=["./","./index.html","./manifest.json","./lander-bg.jpeg","./icon.svg","./apple-touch-icon.png","./icon-192.png","./icon-512.png","./icon-1024.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
