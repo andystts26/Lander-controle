5837e0c792c978c02eb01dbd1c6ed6499c3243c8
andystts26 <andersonibc01@gmail.com>
2026-07-14 01:12:37 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const C="lander-v2-futurista";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","manifest.json","icon.svg","lander-bg.jpeg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="lander-v3-layout-final";
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./lander-bg.jpeg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+ if(e.request.mode==="navigate"){
+  e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put("./index.html",x));return r}).catch(()=>caches.match("./index.html")));
+ }else{
+  e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));
+ }
+});
