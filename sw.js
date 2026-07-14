8e13397f13aed9d2cb42de71b6422ab7d0520512
andystts26 <andersonibc01@gmail.com>
2026-07-14 01:36:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="lander-v6-icones-verificados";
-const A=["./","./index.html","./manifest.json","./icon.svg","./lander-bg.jpeg","./lander-logo.jpeg"];
+const C="lander-ref-exata-v103";
+const A=["./","./index.html","./manifest.json","./icon.svg","./lander-bg.jpeg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
